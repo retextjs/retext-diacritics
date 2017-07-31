1a2e2ef3cb2fb7c4d27bff414d986490e1e4adfe
Titus Wormer <tituswormer@gmail.com>
2017-07-31 19:17:45 -0400

    Fix typo

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var test = require('tape');
 var retext = require('retext');
 var diacritics = require('./');
 
-test('simplify', function (t) {
+test('diacritics', function (t) {
   t.plan(2);
 
   retext()
