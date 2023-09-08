9f90be5239baf1314f8dfc7b3181506240450e3e
Titus Wormer <tituswormer@gmail.com>
2023-09-08 11:19:08 +0200

    Change to use `exports`

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {retext} from 'retext'
-import retextDiacritics from './index.js'
+import retextDiacritics from 'retext-diacritics'
 
 test('retext-diacritics', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('retext-diacritics')).sort(), [
       'default'
     ])
   })
